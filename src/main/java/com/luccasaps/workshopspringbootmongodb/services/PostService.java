dfd76cc2c1447871f499ba484bc48a67ec17f908
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-12 12:53:07 -0300

    Obtendo um post por id

---
+++ src/main/java/com/luccasaps/workshopspringbootmongodb/services/PostService.java
@@ -0,0 +1,22 @@
+package com.luccasaps.workshopspringbootmongodb.services;
+
+import com.luccasaps.workshopspringbootmongodb.domain.Post;
+import com.luccasaps.workshopspringbootmongodb.domain.User;
+import com.luccasaps.workshopspringbootmongodb.repository.PostRepository;
+import com.luccasaps.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PostService {
+
+    @Autowired
+    private PostRepository repo;
+
+    public Post findById(String id) {
+        Optional<Post> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+}
