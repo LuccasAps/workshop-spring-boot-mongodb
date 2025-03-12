4065364e23d956c0b0d5e230e2c66e119cc07ed8
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-12 12:33:34 -0300

    Projeção dos dados do autor com DTO

--- src/main/java/com/luccasaps/workshopspringbootmongodb/config/Instantiation.java
+++ src/main/java/com/luccasaps/workshopspringbootmongodb/config/Instantiation.java
@@ -2,6 +2,7 @@ package com.luccasaps.workshopspringbootmongodb.config;
 
 import com.luccasaps.workshopspringbootmongodb.domain.Post;
 import com.luccasaps.workshopspringbootmongodb.domain.User;
+import com.luccasaps.workshopspringbootmongodb.dto.AuthorDTO;
 import com.luccasaps.workshopspringbootmongodb.repository.PostRepository;
 import com.luccasaps.workshopspringbootmongodb.repository.UserRepository;
 import com.luccasaps.workshopspringbootmongodb.services.UserService;
@@ -35,10 +36,12 @@ public class Instantiation implements CommandLineRunner {
         User alex = new User(null, "Alex Green", "alex@gmail.com");
         User bob = new User(null, "Bob Grey", "bob@gmail.com");
 
-        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para São Paulo. Abraços!", maria);
-        Post post2 = new Post(null, sdf.parse("2018/03/23"),"Bom dia","Acordei feliz hoje!", maria);
-
         userRepository.saveAll(Arrays.asList(maria,alex,bob));
+
+
+        Post post1 = new Post(null, sdf.parse("21/03/2018"),"Partiu Viagem", "Vou viajar para São Paulo. Abraços!", new AuthorDTO(maria));
+        Post post2 = new Post(null, sdf.parse("2018/03/23"),"Bom dia","Acordei feliz hoje!", new AuthorDTO(maria));
+
         postRepository.saveAll(Arrays.asList(post1,post2));
     }
 }
