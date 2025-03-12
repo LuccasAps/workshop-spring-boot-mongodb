253bc4b67f9ef07354d3b9dcd642d0014e29467b
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-12 13:50:08 -0300

    Consulta simples com query methods

--- src/main/java/com/luccasaps/workshopspringbootmongodb/repository/PostRepository.java
+++ src/main/java/com/luccasaps/workshopspringbootmongodb/repository/PostRepository.java
@@ -1,7 +1,11 @@
 package com.luccasaps.workshopspringbootmongodb.repository;
 
 import com.luccasaps.workshopspringbootmongodb.domain.Post;
+import com.luccasaps.workshopspringbootmongodb.dto.AuthorDTO;
 import org.springframework.data.mongodb.repository.MongoRepository;
 
+import java.util.List;
+
 public interface PostRepository extends MongoRepository<Post, String> {
+    List<Post> findByTitleContainingIgnoreCase(String text);
 }
