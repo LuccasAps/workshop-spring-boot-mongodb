91185bfc0233c85af82ac9ed8401ae5a38c0268c
Luccas Asaphe <167210535+LuccasAps@users.noreply.github.com>
2025-03-12 11:16:52 -0300

    Inserção de usuário com POST

--- src/main/java/com/luccasaps/workshopspringbootmongodb/resources/UserResource.java
+++ src/main/java/com/luccasaps/workshopspringbootmongodb/resources/UserResource.java
@@ -6,13 +6,10 @@ import com.luccasaps.workshopspringbootmongodb.dto.UserDTO;
 import com.luccasaps.workshopspringbootmongodb.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.util.ArrayList;
-import java.util.Arrays;
+import java.net.URI;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -36,4 +33,13 @@ public class UserResource {
 
         return ResponseEntity.ok().body(new UserDTO(obj));
     }
+
+    @RequestMapping(method = RequestMethod.POST)
+    public ResponseEntity<Void> insert(@RequestBody UserDTO objDto){
+        User obj = service.fromDto(objDto);
+        obj = service.insert(obj);
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(obj.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
 }
--- src/main/java/com/luccasaps/workshopspringbootmongodb/services/UserService.java
+++ src/main/java/com/luccasaps/workshopspringbootmongodb/services/UserService.java
@@ -1,6 +1,7 @@
 package com.luccasaps.workshopspringbootmongodb.services;
 
 import com.luccasaps.workshopspringbootmongodb.domain.User;
+import com.luccasaps.workshopspringbootmongodb.dto.UserDTO;
 import com.luccasaps.workshopspringbootmongodb.repository.UserRepository;
 import com.luccasaps.workshopspringbootmongodb.services.exception.ObjectNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -22,5 +23,13 @@ public class UserService {
     public User findById(String id) {
         Optional<User> obj = repo.findById(id);
         return obj.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+    }
+
+    public User insert(User obj){
+        return repo.insert(obj);
+    }
+
+    public User fromDto(UserDTO objDto){
+        return new User(objDto.getId(), objDto.getName(), objDto.getEmail());
     }
 }
